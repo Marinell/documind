d7c80264c184830e8254d87d41e517a9695df21b
Marco Marinelli <marco1-marinelli@libero.it>
2025-08-29 17:04:30 +0200

    bump version

--- src/main/webui/src/app/app.component.ts
+++ src/main/webui/src/app/app.component.ts
@@ -12,5 +12,5 @@ import { CommonModule } from '@angular/common'; // Import CommonModule
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Documind - v0.0.1-beta';
+  title = 'Documind - v0.0.2-beta';
 }
